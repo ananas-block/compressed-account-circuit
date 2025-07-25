7c27d86fdb939ebae449614ed2df2001e463b98b
ananas-block <jorrit@lightprotocol.com>
2025-07-25 16:01:24 +0200

    remove address and lamports from compressed account circuit

--- circuits/compressed_account.circom
+++ circuits/compressed_account.circom
@@ -1,35 +1,34 @@
 pragma circom 2.0.0;
 
 include "../node_modules/circomlib/circuits/poseidon.circom";
-
+// No lamports no addresss
 // Circuit for CompressedAccount hashing
 template CompressedAccountHash() {
     // Inputs as field elements
     signal input owner_hashed;
     signal input leaf_index;
     signal input merkle_tree_hashed;
-    signal input lamports;
+    // signal input lamports;
     signal input discriminator;
-    signal input has_data;
+    // signal input has_data;
     signal input data_hash;
 
     // Output
     signal output hash;
 
-    // We'll use Poseidon with 7 inputs
-    component poseidon = Poseidon(7);
+    component poseidon = Poseidon(5);
 
     // Connect inputs to the Poseidon component
     poseidon.inputs[0] <== owner_hashed;
     poseidon.inputs[1] <== leaf_index;
     poseidon.inputs[2] <== merkle_tree_hashed;
-    poseidon.inputs[3] <== lamports + 18446744073709551616; // + lamports domain
-    poseidon.inputs[4] <== discriminator + 36893488147419103232; // + discriminator domain
-    poseidon.inputs[5] <== has_data;
-    poseidon.inputs[6] <== data_hash;
+    // poseidon.inputs[3] <== lamports + 18446744073709551616; // + lamports domain
+    // poseidon.inputs[4] <== address;
+    poseidon.inputs[3] <== discriminator + 36893488147419103232; // + discriminator domain
+    poseidon.inputs[4] <== data_hash;
 
     // Output the Poseidon hash
     hash <== poseidon.out;
 }
 
-component main { public [owner_hashed, leaf_index, merkle_tree_hashed, lamports, discriminator, has_data, data_hash] } = CompressedAccountHash();
+component main { public [owner_hashed, leaf_index, merkle_tree_hashed, discriminator, data_hash] } = CompressedAccountHash();
--- test/compressed_account.test.js
+++ test/compressed_account.test.js
@@ -1,82 +1,81 @@
-const assert = require('assert');
-const fs = require('fs');
-const path = require('path');
+const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
 const circomlibjs = require("circomlibjs");
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-describe('CompressedAccount Circuit Test', function() {
+describe("CompressedAccount Circuit Test", function () {
   this.timeout(60000); // Increase timeout
 
   let poseidon;
 
-  before(async function() {
+  before(async function () {
     // Initialize poseidon
     poseidon = await circomlibjs.buildPoseidonOpt();
   });
 
-  it('should correctly hash compressed account data with poseidon and test the circuit', async function() {
+  it("should correctly hash compressed account data with poseidon and test the circuit", async function () {
     // Hardcoded input values from Rust test
     const hardcodedInputs = [
       "133964189369745375817643650762572252425757995588871671688134800968986291322", // owner_hashed
       "1", // leaf_index
       "274698893731942391252194363483372283063414035850745528064643496257815950661", // merkle_tree_hashed
-      "100", // lamports
-      "38795127103506095060616951389784305450515281095163674791955527140240872547", // address
       "1", // discriminator
-      "3459342586129817037903285055794736781430278749885408056232073615594126477392" // data_hash
+      "3459342586129817037903285055794736781430278749885408056232073615594126477392", // data_hash
     ];
     const hardcodedHashInputs = [
       "133964189369745375817643650762572252425757995588871671688134800968986291322", // owner_hashed
       "1", // leaf_index
       "274698893731942391252194363483372283063414035850745528064643496257815950661", // merkle_tree_hashed
-      "18446744073709551716", // lamports
-      "38795127103506095060616951389784305450515281095163674791955527140240872547", // discriminator
-      "36893488147419103233", // has_data
-      "3459342586129817037903285055794736781430278749885408056232073615594126477392" // data_hash
+      "1", // discriminator
+      "3459342586129817037903285055794736781430278749885408056232073615594126477392", // data_hash
     ];
 
-
     // Convert input strings to BigInt
-    const inputFields = hardcodedInputs.map(value => BigInt(value));
+    const inputFields = hardcodedInputs.map((value) => BigInt(value));
 
     // Log all the inputs
     console.log("Compressed Account Test - Input Values:");
     console.log("owner_hashed:", inputFields[0].toString());
     console.log("leaf_index:", inputFields[1].toString());
     console.log("merkle_tree_hashed:", inputFields[2].toString());
-    console.log("lamports:", inputFields[3].toString());
-    console.log("discriminator:", inputFields[4].toString());
-    console.log("has_data:", inputFields[5].toString());
-    console.log("data_hash:", inputFields[6].toString());
+    console.log("discriminator:", inputFields[3].toString());
+    console.log("data_hash:", inputFields[4].toString());
 
     // Create circuit input
     const circuitInput = {
       owner_hashed: inputFields[0].toString(),
       leaf_index: inputFields[1].toString(),
       merkle_tree_hashed: inputFields[2].toString(),
-      lamports: inputFields[3].toString(),
+      // lamports: inputFields[3].toString(),
       discriminator: inputFields[4].toString(),
-      has_data: inputFields[5].toString(),
-      data_hash: inputFields[6].toString()
+      // has_data: inputFields[5].toString(),
+      data_hash: inputFields[6].toString(),
     };
 
     // Save input to file for reference
     fs.writeFileSync(
       path.join(__dirname, "../compressed_account_input.json"),
-      JSON.stringify(circuitInput, null, 2)
+      JSON.stringify(circuitInput, null, 2),
     );
 
     console.log("\nCircuit input saved to compressed_account_input.json");
 
     // Compute Poseidon hash manually with circomlibjs
-    const poseidonInputs = hardcodedHashInputs.map(x => poseidon.F.e(x.toString()));
+    const poseidonInputs = hardcodedHashInputs.map((x) =>
+      poseidon.F.e(x.toString()),
+    );
     const manualHash = poseidon.F.toString(poseidon(poseidonInputs));
     console.log("\nPoseidon hash (computed manually):", manualHash);
 
     // Try to compile the circuit
     try {
-      await exec('cd ' + path.join(__dirname, '..') + ' && circom circuits/compressed_account.circom --r1cs --wasm --sym');
+      await exec(
+        "cd " +
+          path.join(__dirname, "..") +
+          " && circom circuits/compressed_account.circom --r1cs --wasm --sym",
+      );
       console.log("Circuit compiled successfully");
     } catch (error) {
       console.error("Error compiling circuit:", error.message);
@@ -85,11 +84,21 @@ describe('CompressedAccount Circuit Test', function() {
 
     // Try to run the circuit
     try {
-      await exec('cd ' + path.join(__dirname, '..') + ' && node_modules/.bin/snarkjs wtns calculate compressed_account_js/compressed_account.wasm compressed_account_input.json witness.wtns');
-      await exec('cd ' + path.join(__dirname, '..') + ' && node_modules/.bin/snarkjs wtns export json witness.wtns witness.json');
+      await exec(
+        "cd " +
+          path.join(__dirname, "..") +
+          " && node_modules/.bin/snarkjs wtns calculate compressed_account_js/compressed_account.wasm compressed_account_input.json witness.wtns",
+      );
+      await exec(
+        "cd " +
+          path.join(__dirname, "..") +
+          " && node_modules/.bin/snarkjs wtns export json witness.wtns witness.json",
+      );
 
       // Read the witness
-      const witnessJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../witness.json')));
+      const witnessJson = JSON.parse(
+        fs.readFileSync(path.join(__dirname, "../witness.json")),
+      );
       console.log("Circuit output hash:", witnessJson[1]);
 
       // Compare with expected hash
@@ -97,9 +106,13 @@ describe('CompressedAccount Circuit Test', function() {
       console.log("Circuit computed the correct hash!");
 
       // Provide hash in the format needed for the Rust test
-      console.log("\n----------------------------------------------------------------------");
+      console.log(
+        "\n----------------------------------------------------------------------",
+      );
       console.log("The hash value to use in the Rust test: " + manualHash);
-      console.log("----------------------------------------------------------------------");
+      console.log(
+        "----------------------------------------------------------------------",
+      );
     } catch (error) {
       console.error("Error running circuit:", error.message);
     }
